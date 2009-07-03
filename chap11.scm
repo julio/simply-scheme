efc9d877c2fa3278cf6b40e56db8ddba2df87690
Julio Santos <julio@morgane.com>
2009-07-03 11:15:28 -0700

    first commit

---
+++ chap11.scm
@@ -0,0 +1,20 @@
+;;; (downup 'foo) => (foo fo f fo foo)
+
+(define (downup wd)
+  (if (= (count wd) 1)
+    (se wd)
+    (se wd (downup (bl wd)) wd)))
+
+;;; is the letter a vowel?
+
+(define (vowel? ltr)
+  (member? ltr '(a e i o u)))
+  
+;;; (pigl 'word) => ordway
+
+(define (pigl wd)
+  (if (vowel? (first wd))
+    (word wd 'ay)
+    (pigl (word (bf wd) (first wd)))))
+    
+    
