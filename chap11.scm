43db6810cb6da9f5003c0bc78c86ec5eba8d3bb5
Julio Santos <julio@morgane.com>
2009-07-03 16:24:52 -0700

    phone unspell

--- chap11.scm
+++ chap11.scm
@@ -38,5 +38,26 @@
     0
     (let ((inc (if (eq? 'um (first sent)) 1 0)))
       (+ inc (count-ums (bf sent))))))
-      
+    
+;;; phone-unspell
+;;;
+;;; (phone-unspell 'popcorn) => 7672676
+
+(define (unspell-letter letter)
+  (cond ((member? letter 'abc) 2)
+        ((member? letter 'def) 3)
+        ((member? letter 'ghi) 4)
+        ((member? letter 'jkl) 5)
+        ((member? letter 'mno) 6)
+        ((member? letter 'prs) 7)
+        ((member? letter 'tuv) 8)
+        ((member? letter 'wxyz) 9)
+        (else 0)))
+        
+(define (phone-unspell wd)
+  (if (= 0 (count wd))
+    wd
+    (word (unspell-letter (first wd)) (phone-unspell (bf wd)))))
+    
+  
             
