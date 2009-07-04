b9ee18422ff85869b107c3a7d72636cecaa46975
Julio Santos <julio@morgane.com>
2009-07-04 00:27:58 -0700

    roman numerals

--- chap12.scm
+++ chap12.scm
@@ -31,3 +31,23 @@
   (if (eq? sent '())
     sent
     (se (exaggerate-item (first sent)) (exaggerate (bf sent)))))
+    
+;;; roman numerals
+
+(define (arabic roman)
+  (if (= 0 (count roman))
+    0
+    (if (and (> (count roman) 1)
+             (< (arabic-value (first roman)) (arabic-value (first (bf roman)))))
+      (- (arabic (bf roman)) (arabic-value (first roman)))
+      (+ (arabic-value (first roman)) (arabic (bf roman))))))
+
+(define (arabic-value roman-numeral)
+  (cond ((eq? 'm roman-numeral) 1000)
+        ((eq? 'd roman-numeral) 500)
+        ((eq? 'c roman-numeral) 100)
+        ((eq? 'l roman-numeral) 50)
+        ((eq? 'x roman-numeral) 10)
+        ((eq? 'v roman-numeral) 5)
+        ((eq? 'i roman-numeral) 1)
+        (else 0)))
