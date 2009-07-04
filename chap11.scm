1bbbc926494a2b85d5d65e987a118c9a32877e2a
Julio Santos <julio@morgane.com>
2009-07-03 18:34:41 -0700

    reverse and factorial

--- chap11.scm
+++ chap11.scm
@@ -77,3 +77,17 @@
     '()
     (se wd (copies (- n 1) wd))))
     
+;;; reverse
+
+(define (reverse wd)
+  (if (= 1 (count wd))
+    wd
+    (word (last wd) (reverse (bl wd)))))
+
+;;; factorial
+
+(define (factorial n)
+  (if (= 1 n)
+    1
+    (* n (factorial (- n 1)))))
+    
