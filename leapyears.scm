8f2c5f4d309537d9e8c9235939e9cfbd05f38060
Julio Santos <julio@morgane.com>
2009-07-04 01:17:23 -0700

    renames

--- leapyears.scm
+++ leapyears.scm
@@ -4,9 +4,9 @@
     (- (truncate (/ year 400)) 
        (truncate (/ year 100)))))
 
-(define (number-of-leap-years year1 year2)
+(define (leap-years-between year1 year2)
   (let 
-    ((start-year-offset (if (leap-year? year1) 1 0)))
+    ((inc (if (leap-year? year1) 1 0)))
     (+
       (- (leapyears-to-year year2) (leapyears-to-year year1))
-      start-year-offset)))
+      inc)))
