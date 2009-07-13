da51f1de59355ce5edab8155afc94f355d37fe6d
Julio Santos <julio@morgane.com>
2009-07-12 20:24:51 -0700

    pairs

--- huge-numbers.scm
+++ huge-numbers.scm
@@ -1,10 +1,10 @@
-;;; TODO
-;;;  numbers are sometimes "000" instead of '000 or (000) and need to add 0 to fix
-;;;  big names need to offset first position. using "nothing" for now
+;;; converts a number of plain english. goes all the way to decillion
 (define (huge-number n)
   (let ((numbers (number-to-groups-of-three n)))
     (huge-number-helper numbers (count numbers))))
 
+;;; traverses a list of 3 digit groups, converting each group to 
+;;; words, based on each group's position
 (define (huge-number-helper groups-of-numbers number-of-groups)
   (if (<= (count groups-of-numbers) 1)
     (say-three-digit-number (first groups-of-numbers)) ;; "first" to get the actual word (number), not the sentence
@@ -14,10 +14,10 @@
       (huge-number-helper 
         (bf groups-of-numbers) 
         (count (bf groups-of-numbers))))))
-  
+
 ;;; order of magnitude based on position
 (define (big-names n position)
-  (if (= 0 (+ 0 n))
+  (if (= 0 (to-number n))
     '()
     (item position '(nothing thousand million billion trillion quadrillion sextillion octillion nonillion decillion))))
 
@@ -31,23 +31,23 @@
 (define (say-three-digit-number sent)
   (cond ((< sent 1)    '())
         ((< sent 10)   (se (say-singles sent)))
-        ((< sent 100)  (se (say-tens (+ 0 sent))))
+        ((< sent 100)  (se (say-tens (to-number sent))))
         ((< sent 1000) (se (say-singles (first sent)) 'hundred (say-three-digit-number (bf sent))))
-        (else 'error1)))
+        (else (display-error 1 sent))))
 
 ;;; Say 1, 2, ... 9
 (define (say-singles n)
-  (cond ((eq? '0 (+ 0 n)) '())
-        ((eq? '1 (+ 0 n)) 'one)
-        ((eq? '2 (+ 0 n)) 'two)
-        ((eq? '3 (+ 0 n)) 'three)
-        ((eq? '4 (+ 0 n)) 'four)
-        ((eq? '5 (+ 0 n)) 'five)
-        ((eq? '6 (+ 0 n)) 'six)
-        ((eq? '7 (+ 0 n)) 'seven)
-        ((eq? '8 (+ 0 n)) 'eight)
-        ((eq? '9 (+ 0 n)) 'nine)
-        (else (se 'error2 '- n))))
+  (cond ((eq? '0 (to-number n)) '())
+        ((eq? '1 (to-number n)) 'one)
+        ((eq? '2 (to-number n)) 'two)
+        ((eq? '3 (to-number n)) 'three)
+        ((eq? '4 (to-number n)) 'four)
+        ((eq? '5 (to-number n)) 'five)
+        ((eq? '6 (to-number n)) 'six)
+        ((eq? '7 (to-number n)) 'seven)
+        ((eq? '8 (to-number n)) 'eight)
+        ((eq? '9 (to-number n)) 'nine)
+        (else (display-error 2 n))))
 
 ;;; Say 10, 11, ..., 20, 21, ... 99
 (define (say-tens nn)
@@ -61,7 +61,7 @@
         ((eq? '7 (first nn)) (se 'seventy (say-singles (bf nn))))
         ((eq? '8 (first nn)) (se 'eighty  (say-singles (bf nn))))
         ((eq? '9 (first nn)) (se 'ninety  (say-singles (bf nn))))
-        (else 'error3)))
+        (else (display-error 3 nn))))
 
 ;;; Say 10, 11, 12, ... 19
 (define (say-teens nn)
@@ -75,4 +75,18 @@
         ((eq? '17 nn) 'seventeen)
         ((eq? '18 nn) 'eighteen)
         ((eq? '19 nn) 'nineteen)
-        (else 'error4)))
+        (else (display-error 4 nn))))
+
+
+;;; TODO
+;;;  numbers are sometimes "000" instead of '000 or (000) and need to add 0 to fix
+;;;  big names need to offset first position. using "nothing" for now
+
+;;; some of the groups go from 'nnn to "nnn" which screws up comparisons.
+;;; adding 0 to "nnn" returns 'nnn, which helps. Ideally we would not need
+;;; to do this.
+(define (to-number n)
+  (+ 0 n))
+
+(define (display-error number msg)
+  (se '/error number msg '/))
